e14efa6f6f37c22f774a9e4bb432f2f2ef415c0a
Wooseok <rladntjr9808@gmail.com>
2023-02-19 22:15:52 +0900

    Update Variable

---
+++ Variable/1_variable.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h> 
+//<bits/stdc++.h> 헤더파일을 포함하라는 전처리기를 의미
+// 혹은 선행처리기 , preprocessor 라고함.
+using namespace std;
+
+// Data Type을 의미 Data Type 변수명 과 같은 구조
+// 변수명은 앞에 숫자가 올 수 없음.
+int a;
+
+int main(void)
+{
+    a = 10;
+    cout << &a << '\n';
+    return 0;
+}
+
+/*
+* 변수(variable): 데이터를 저장하는 메모리 공간을 의미함.
+* 
+*/
---
+++ Variable/2_variable.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 데이터의 종류: 자료형
+// 정수형 , 부동 소수점 , 문자형 , 부울 로 나눌수 있고 타입에 따라 데이터 저장가능.
+// 문자형의 경우 하나의 문자 저장가능
+
+/*
+* 정수형: short , int , long , long long
+* long long 타입은 64bit로 정수 표시 -> 큰범위의 정수 표현 가능.
+* 부동 소수점: float , double , long double
+* float: 32bit, double , long double: 64bit
+*/
+
+string a;
+int main()
+{
+    a = "hi";
+    cout << a << '\n';
+    return 0;
+}
